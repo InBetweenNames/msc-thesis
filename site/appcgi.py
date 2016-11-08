26f94c32340a90fd72255cfe51e7c1ec153f6ae4
Shane Peelar <lookatyouhacker@gmail.com>
2016-11-07 20:35:32 -0500

    Add rest of site

---
+++ site/appcgi.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import sys;
+import cgi;
+import cgitb;
+import subprocess;
+import os;
+
+#cgitb.enable(); #Enable for debugging
+
+print("Content-Type: text/html\n\n")
+
+#os.chdir("/home/solarman")
+
+post = cgi.FieldStorage()
+query = post["query"].value;
+
+
+#p = subprocess.check_output(["whoami"])
+
+#print(p.decode("utf-8"))
+
+os.environ["HOME"] = "/var/www"
+proc = subprocess.Popen(["/opt/ghc8/bin/ghc", "Interactive.hs", "-e", ":set -XSafe", "-e", query], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+#sys.stdout.write("<pre>")
+for lineu in proc.stdout:
+  line=lineu.decode("utf-8")
+  sys.stdout.write(line)
+for lineu in proc.stderr:
+  line=lineu.decode("utf-8")
+  sys.stdout.write(line)
+#sys.stdout.write("</pre>")
+
+proc.wait()
+
+#print(p.decode("utf-8"))
